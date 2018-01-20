631abf56f423f77c7ae87d63ef1dacf5b2a9c510
Digized <Zuraiz.zafar97@gmail.com>
2018-01-20 00:10:27 -0500

    number-gen.py

--- number-gen.py
+++ number-gen.py
@@ -1,9 +1,26 @@
 import random
 
+def chosenValue(playAgain):
+    if(playAgain == "yes"):
+        return "yes"
+    elif(playAgain == "no"):
+        return "no"
+    else:
+        playAgain = input("Please Try Again\n")
+        return chosenValue(playAgain)
+
+
 playAgain = input("Do you want to play?\n")
-while( playAgain == "yes"):
+playAgain = chosenValue(playAgain)
+while(playAgain == "yes" and playAgain != "no"):
     a = int(input("What is minimum number: "))
     b = int(input("What is the maximum number: "))
     randomGen = random.randint(a, b)
     print("Dice rolled a", randomGen)
     playAgain = input("Do you want to play again?\n")
+    playAgain = chosenValue(playAgain)
+
+
+
+
+
