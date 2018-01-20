7bc69b06c6f968577a95407a0c5719b19d05b471
Digized <Zuraiz.zafar97@gmail.com>
2018-01-19 23:33:59 -0500

    while loop

--- number-gen.py
+++ number-gen.py
@@ -1,9 +1,9 @@
 import random
 
-a = int(input("What is minimum number"))
-b = int(input("What is the maximum number"))
-
-randomGen = random.randint(a, b)
-
-print("Dice rolled a", randomGen)
-
+playAgain = input("Do you want to play?\n")
+while( playAgain == "yes"):
+    a = int(input("What is minimum number: "))
+    b = int(input("What is the maximum number: "))
+    randomGen = random.randint(a, b)
+    print("Dice rolled a", randomGen)
+    playAgain = input("Do you want to play again?\n")
